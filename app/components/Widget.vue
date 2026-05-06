6f44d929412f92b6bbdd1ec1cf3dc31d343cc68e
Mosoonk <inermetso@hotmail.com>
2026-05-06 19:37:02 +0200

    create WeatherWidget

---
+++ app/components/Widget.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <div class="container">
+      <h2>Historisches Wetter in Oelsnitz/Vogtland</h2>
+      <p>Wähle ein Datum, um die Durchschnittstemperatur anzuzeigen</p>
+
+      <div class="weather-controls">
+      </div>
+    </div>
+  </section>
+</template>
